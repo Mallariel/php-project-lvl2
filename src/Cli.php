c56ff35a30cfa2adaf41e3e65e872f6ba18127ef
Mallariel <aleksey.khomenko.95@gmail.com>
2021-08-26 18:12:57 +0300

    add: option --format

--- src/Cli.php
+++ src/Cli.php
@@ -12,10 +12,12 @@ function run()
     Usage:
       gendiff (-h|--help)
       gendiff (-v|--version)
+      gendiff [--format <fmt>] <firstFile> <secondFile>
 
     Options:
       -h --help                     Show this screen
       -v --version                  Show version
+      --format <fmt>                Report format [default: stylish]
     DOC;
 
     $args = Docopt::handle($doc, array('version'=>'Gendiff 0.0.1'));
